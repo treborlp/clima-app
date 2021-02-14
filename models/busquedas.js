6f860012d4c7cb2d3d05e5ea71cee24455f0a9ff
Robert Alvarado <treborlp@gmail.com>
2021-02-13 21:54:02 -0500

    Instacias en maxbox

--- models/busquedas.js
+++ models/busquedas.js
@@ -7,11 +7,29 @@ class Busquedas {
         //TODO: leer si existe en db
     }
 
+    get paramsMaxbox() {
+        return {
+            'access_token': 'pk.eyJ1IjoidHJlYm9ybHAiLCJhIjoiY2tsNGo4YXI5MHR0ZTJ2cDZpZXEyc29qdyJ9.4kEISb80PwvkahSw_jr70Q',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
     async ciudad(lugar = '') {
         //peticion http
+        try {
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMaxbox
+            })
 
-        const peticion = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Madrid.json?access_token=pk.eyJ1IjoidHJlYm9ybHAiLCJhIjoiY2tsNGo4YXI5MHR0ZTJ2cDZpZXEyc29qdyJ9.4kEISb80PwvkahSw_jr70Q&limit=5&language=es');
-        console.log(peticion.data);
+            const peticion = await instance.get();
+            console.log(peticion.data);
+
+            return []
+        } catch (error) {
+            return []
+        }
 
 
         //return []; // Retornar lugares similares
